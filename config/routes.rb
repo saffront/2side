1efe8aad52854253b542a84cf903908b5849cdc9
saffront <jiashern@hotmail.com>
2015-11-27 16:23:17 +0000

    updated routes with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, ccontrollers: { registrations: 'users/registrations'}
+  resources :contacts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   get '/about' => 'pages#about'
-
-  resources :contacts
 
   # You can have the root of your site routed with "root"
  root 'pages#home'
