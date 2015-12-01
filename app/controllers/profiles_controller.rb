fba02a98e845bbd18bc4a90a1131ab5ea903692d
saffront <jiashern@hotmail.com>
2015-12-01 17:13:54 +0000

    added _form and edit func

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,12 +3,6 @@ class ProfilesController < ApplicationController
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
     @profile = Profile.new
-
-  def edit
-  @user = User.find( params[:user_id] )
-  @profile = @user.profile
-  end
-
   end
   def create 
     @user = User.find( params[:user_id] )
@@ -20,6 +14,12 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+
+    def edit
+      @user = User.find( params[:user_id] )
+      @profile = @user.profile
+    end
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
