638d4f66faf4b39b82a887a0f3923a90f7e3c523
saffront <jiashern@hotmail.com>
2015-12-01 01:06:43 +0000

    added profiles form and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations'}
-  resources :contacts
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
-    resources :profile
+    resource :profile
   end
+  resources :contacts
+
+  get '/about' => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get '/about' => 'pages#about'
 
   # You can have the root of your site routed with "root"
  root 'pages#home'
