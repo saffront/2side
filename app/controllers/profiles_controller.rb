79379d94fa9406351eeeb6550f9a11183bf3a103
saffront <jiashern@hotmail.com>
2015-12-01 00:47:22 +0000

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,8 @@
 class ProfilesController < ApplicationController
+	def new
+		#form where user can fill out profile
+		@user = User.find( params[:user_id] )
+		@variable = params [:hello]
+		@profile = @user.build_profile
+	end
 end
